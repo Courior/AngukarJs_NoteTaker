be6f526af8568cdc78c1288e220064ffa70a645d
Courior <Courior@users.noreply.github.com>
2016-05-11 16:29:13 +0100

    basic controller to output multible notes

--- app/app.js
+++ app/app.js
@@ -6,7 +6,8 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.version',
-  'myApp.note'
+  'myApp.note',
+  'myApp.noteColumnNew'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
