b60dc49cb594d1d32bc02b4f0accde68d0288b5f
Courior <Courior@users.noreply.github.com>
2016-05-13 11:37:48 +0100

    animating a chevron

--- app/components/icons/icon.js
+++ app/components/icons/icon.js
@@ -7,5 +7,11 @@ angular.module('myApp.icon', [])
     replace: false,
     templateUrl:'components/icons/icon.html'
   };
+}).directive('chevronIcon', function(){
+  return {
+    restrict: 'E',
+    replace: false,
+    templateUrl:'components/icons/chevron.html'
+  };
 });
 //title="Please Work" byline="i hope this works" description="did it work?"
